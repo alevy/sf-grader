87a2ba1c4ad5bc5b26312957688b84b0b0a2c4d0
Amit Aryeh Levy <amit@amitlevy.com>
2022-07-16 20:34:44 -0700

    Use Blobs for grading and submission tarballs

--- functions/cos326_grader/workload.py
+++ functions/cos326_grader/workload.py
@@ -19,38 +19,65 @@ def handle(req, syscall):
     return result
 
 def app_handle(args, state, syscall):
+    """Compiles and runs an assignment-specific grading script for an assignment submission
+
+    Parameters
+    ----------
+    args : dict
+        A dictionary containing a reference to a gzipped tarball of the submission
+        under the "submission" key
+    state : dict
+        A dictionary containing the assignment name (e.g. "a1", "a2", etc) under
+        state["metadata"]["assignment"]. This is most likely set in the `_meta`
+        repository prefix. This *must* correspond to a key in the 'cos326-f22/assignments'
+        key containing a grading_script pointing to the grading script gzipped tarball.
+    syscall : Syscall object
+    """
     assignment = state["metadata"]["assignment"]
+    # Assignment definitions are under {github org}/assignments as a JSON string
+    # that includes a "grading_script" key for each assignment
+    assignments_def = json.loads(syscall.read_key(bytes(f'{state["repository"].split("/")[0]}/assignments', 'utf-8')).decode('utf8'))
+    assignment_grading_script_ref = assignments_def[assignment]["grading_script"]
+    grading_script = syscall.read_key(bytes(assignment_grading_script_ref, 'utf-8'))
 
     with tempfile.TemporaryDirectory() as workdir:
         shutil.copy("/srv/utils326.ml", workdir)
         os.chdir(workdir)
         os.putenv("OCAMLLIB", "/srv/usr/lib/ocaml")
 
-        with tempfile.NamedTemporaryFile(suffix=".tar.gz") as submission_tar:
-            submission_tar_data = syscall.read_key(bytes(args["submission"], "utf-8"))
-            submission_tar.write(submission_tar_data)
-            submission_tar.flush()
+        with syscall.open_unnamed(args["submission"]) as submission_tar_file:
             os.mkdir("submission")
-            os.system("tar -C submission -xzf %s --strip-components=1" % submission_tar.name)
-            os.putenv("SUBMISSION_DIR", "submission")
-            with tempfile.NamedTemporaryFile(suffix=".tar.gz") as script_tar:
-                script_tar_data = syscall.read_key(bytes("cos326/%s/grading_script" % assignment, "utf-8"))
-                script_tar.write(script_tar_data)
-                script_tar.flush()
-                os.system("tar -C submission -xzf %s" % script_tar.name)
+            tarp = subprocess.Popen("tar -C submission -xz --strip-components=1", shell=True, stdin=subprocess.PIPE)
+            bs = submission_tar_file.read()
+            while len(bs) > 0:
+                tarp.stdin.write(bs)
+                bs = submission_tar_file.read()
+            tarp.stdin.close()
+            tarp.communicate()
+        os.system("ls submission")
+        os.putenv("SUBMISSION_DIR", "submission")
 
-                compilerun = subprocess.Popen(f"PATH=/srv/usr/bin:{os.environ['PATH']} make -sef submission/Makefile", shell=True,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                compileout, compileerr = compilerun.communicate()
+        with syscall.open_unnamed(grading_script) as grading_script_tar_file:
+            tarp = subprocess.Popen("tar -C submission -xz", shell=True, stdin=subprocess.PIPE)
+            bs = grading_script_tar_file.read()
+            while len(bs) > 0:
+                tarp.stdin.write(bs)
+                bs = grading_script_tar_file.read()
+            tarp.stdin.close()
+            tarp.communicate()
+            os.system("ls submission")
 
-                if compilerun.returncode != 0:
-                    return { "error": { "compile": compileerr.decode("utf-8"), "returncode": compilerun.returncode } }
+        compilerun = subprocess.Popen(f"PATH=/srv/usr/bin:{os.environ['PATH']} make -sef submission/Makefile", shell=True,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        compileout, compileerr = compilerun.communicate()
+
+        if compilerun.returncode != 0:
+            return { "error": { "compile": compileerr.decode("utf-8"), "returncode": compilerun.returncode } }
 
         testrun = subprocess.Popen("/srv/usr/bin/ocamlrun a.out 2>&1", shell=True,
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         testout, testerr = testrun.communicate()
 
-        report_key = args['submission'] + "/report"
+        report_key = f"github/{state['repository']}/{state['commit']}/report"
         syscall.write_key(report_key.encode('utf-8'), testout)
         return { "report": report_key, "test_stderr": testerr.decode('utf-8') }
-
--- functions/gh_repo/workload.py
+++ functions/gh_repo/workload.py
@@ -5,6 +5,7 @@ import time
 
 def handle(req, syscall):
     key = "github/%s/%s.tgz" % (req["repository"]["full_name"], req["after"])
+    branch_key = "github/%s/%s.tgz" % (req["repository"]["full_name"], req["ref"])
     meta_key = "github/%s/_meta" % (req["repository"]["full_name"])
     workflow_key = "github/%s/_workflow" % (req["repository"]["full_name"])
 
@@ -13,14 +14,15 @@ def handle(req, syscall):
         metadata = json.loads(metadataString)
         workflow = json.loads(syscall.read_key(bytes(workflow_key, "utf-8")) or "[]")
 
-        resp = syscall.github_rest_get("/repos/%s/tarball/%s" % (req["repository"]["full_name"], req["after"]));
+        resp = syscall.github_rest_get("/repos/%s/tarball/%s" % (req["repository"]["full_name"], req["after"]), toblob=True);
         syscall.write_key(bytes(key, "utf-8"), resp.data)
+        syscall.write_key(bytes(branch_key, "utf-8"), resp.data)
 
         if len(workflow) > 0:
             next_function = workflow.pop(0)
             syscall.invoke(next_function, json.dumps({
                 "args": {
-                    "submission": key
+                    "submission": resp.data.decode('utf-8')
                 },
                 "workflow": workflow,
                 "context": {
